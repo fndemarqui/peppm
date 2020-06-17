116ec052102363cbf168782aae15718f2bdb914b
fndemarqui <fndemarqui@gmail.com>
2020-06-17 10:55:52 -0300

    uploading everything

--- inst/script_development.R
+++ inst/script_development.R
@@ -15,16 +15,13 @@ devtools::check()
 devtools::test()
 
 # devtools::check_win_devel()
-# rhub::check_for_cran()
+rhub::check_for_cran()
 
 
 devtools::build()
 devtools::build_manual()
 devtools::release_checks()
 devtools::spell_check()
-# devtools::test()
-# devtools::reload()
-# devtools::run_examples()
 
 
 git add .
