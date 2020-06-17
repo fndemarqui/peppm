3fddf6490be9bd19950dc7014413900abe3f2988
fndemarqui <fndemarqui@gmail.com>
2020-06-17 10:47:45 -0300

    CRAN submission

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 R package to fit the Piecewise Exponential distribution with random time grids using the clustering structure of the Product Partition Models.
 
 Installation: devtools::install_github("fndemarqui/peppm")
+
+<!-- badges: start -->
+  [![Travis build status](https://travis-ci.com/fndemarqui/peppm.svg?branch=master)](https://travis-ci.com/fndemarqui/peppm)
+  <!-- badges: end -->
--- inst/script_development.R
+++ inst/script_development.R
@@ -1,15 +1,21 @@
 
 
-usethis::use_build_ignore("inst/script_development.R")
 
 
-usethis::use_travis()
+
+#usethis::use_travis()
 devtools::install()
 devtools::document()
+
+usethis::use_build_ignore("inst/script_development.R")
+usethis::use_build_ignore(".travis.yml")
+# usethis::use_cran_comments()
+
 devtools::check()
 devtools::test()
-devtools::check_win_devel()
-rhub::check_for_cran()
+
+# devtools::check_win_devel()
+# rhub::check_for_cran()
 
 
 devtools::build()
@@ -22,5 +28,8 @@ devtools::spell_check()
 
 
 git add .
-git commit
+git commit -m "cran submission"
+git push --set-upstream origin master
 
+
+git remote add origin https://github.com/fndemarqui/peppm.git
